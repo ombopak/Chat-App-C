1fe0b33a5ce8bc61e7517fb6a823216c35e01243
msyndev <msyndev@ombopak.com>
2022-12-30 12:00:39 +0700

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,294 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class MainWindow : Window
+    {
+        TcpClient client;
+        NetworkStream stream;
+        public ObservableCollection<string> Userlist { get; set; }
+        public ObservableCollection<string> MessageList { get; set; }
+        public ConcurrentDictionary<string, List<string>> chatHistories { get; set; }
+
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            MessageList = new ObservableCollection<string>();
+            Userlist = new ObservableCollection<string>();
+            chatHistories = new ConcurrentDictionary<string, List<string>>();
+
+
+            btnLogin.Content = "LOGIN";
+
+
+        }
+
+
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Connect to the server
+                client = new TcpClient();
+                await client.ConnectAsync(IPAddress.Parse("127.0.0.1"), 7891);
+
+                string username = UsernameTextBox.Text;
+
+                stream = client.GetStream();
+
+                //TODO:GET PORT FOR IDENTIFIER UNIQUE
+                byte[] dataToServer = Encoding.UTF8.GetBytes("USERNAME:" + username);
+                await stream.WriteAsync(dataToServer, 0, dataToServer.Length);
+
+                // Start a new thread to continuously read data from the server
+                Thread thread = new Thread(new ThreadStart(ReceiveDataFromServer));
+                thread.Start();
+            }
+            catch
+            {
+                MessageBox.Show("Could not connect to server.");
+            }
+
+
+        }
+
+        private async void ReceiveDataFromServer()
+        {
+            // Continuously read data from the server
+            byte[] dataFromServer = new byte[1024];
+            while (true)
+            {
+                int dataRead = await stream.ReadAsync(dataFromServer, 0, dataFromServer.Length);
+                string message = Encoding.UTF8.GetString(dataFromServer, 0, dataRead);
+                string[] messagePart = message.Split(':');
+
+                if (messagePart[0] == "USERNAME")
+                {
+
+                    string[] newUserList = messagePart[1].Split(',');
+                    App.Current.Dispatcher.Invoke(() => Userlist.Clear());
+
+                    string username;
+
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        username = UsernameTextBox.Text;
+                        foreach (string user in newUserList)
+                        {
+                            if (user != username)
+                            {
+                                App.Current.Dispatcher.Invoke(() =>
+                       Userlist.Add(user));
+                            }
+
+                        }
+                    }
+                   );
+
+                }
+                else if (messagePart[0] == "DISCONNECT")
+                {
+                    string[] newUserList = messagePart[1].Split(',');
+                    App.Current.Dispatcher.Invoke(() => Userlist.Clear());
+                    //Send updated user list after delete
+
+                    string username;
+
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        username = UsernameTextBox.Text;
+                        foreach (string user in newUserList)
+                        {
+                            if (user != username)
+                            {
+                                App.Current.Dispatcher.Invoke(() =>
+                       Userlist.Add(user));
+                            }
+
+                        }
+                    }
+                  );
+                }
+                else if (messagePart[0] == "MESSAGE")
+                {                   
+                    //sender message username
+                    string key = messagePart[1];
+                    //receiver message username
+                    string values = messagePart[2];
+
+
+                    // Add the client to the list
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                            if (chatHistories.ContainsKey(key))
+                            {
+                                chatHistories[key].Add(values);
+                            }
+                            else
+                            {
+                                chatHistories.TryAdd(key, new List<string> { values });
+                            }
+                        
+                    });
+
+                    App.Current.Dispatcher.Invoke( () => MessageList.Clear());
+
+
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (UserListBox.SelectedItem != null)
+                        {
+                            string recipientM = (string)UserListBox.SelectedItem;
+                            string senderM = UsernameTextBox.Text;
+                            string key;
+
+                            if (string.Compare(senderM, recipientM) < 0)
+                            {
+                                key = senderM + "_" + recipientM;
+                            }
+                            else
+                            {
+                                key = recipientM + "_" + senderM;
+                            }
+
+                            List<string> msgs;
+                            if (chatHistories.TryGetValue(key, out msgs))
+                            {
+
+                                foreach (var item in msgs)
+                                {
+                                   
+                                        MessageList.Add(item);
+                                    
+
+                                }
+                            }
+
+                        }
+
+                    });
+
+
+                }
+
+            }
+        }
+
+        private async void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            await App.Current.Dispatcher.Invoke(async () =>
+            {
+                if (UserListBox.SelectedItem != null)
+                {
+
+                    string recipientM = (string)UserListBox.SelectedItem;
+                    string senderM = UsernameTextBox.Text;
+                    string key;
+
+                    Label2.Content = recipientM;
+
+                    if (string.Compare(senderM, recipientM) < 0)
+                    {
+                        key = senderM + "_" + recipientM;
+                    }
+                    else
+                    {
+                        key = recipientM + "_" + senderM;
+                    }
+                    
+                    string message = senderM + " | " + MessageTextBox.Text;
+
+
+                    byte[] dataToServer = Encoding.UTF8.GetBytes("MESSAGE:" + key + ":" + message);
+
+
+                    await stream.WriteAsync(dataToServer, 0, dataToServer.Length);
+
+                    MessageTextBox.Text = "";
+
+                }
+                else { MessageBox.Show("Pick user from user list"); }
+
+            });
+
+
+
+
+        }
+
+        private async void Window_Closed(object sender, EventArgs e)
+        {
+            byte[] dataToServer = Encoding.UTF8.GetBytes("DISCONNECT:" + UsernameTextBox.Text);
+            await stream.WriteAsync(dataToServer, 0, dataToServer.Length);
+        }
+
+     
+        private async void UserListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await App.Current.Dispatcher.Invoke(async () => MessageList.Clear());
+
+            await App.Current.Dispatcher.Invoke(async () =>
+            {
+                if (UserListBox.SelectedItem != null)
+            {
+                string recipientM = UserListBox.SelectedItem.ToString();
+                string senderM = UsernameTextBox.Text;
+                string key;
+
+                Label2.Content = recipientM;
+
+                if (string.Compare(senderM, recipientM) < 0)
+                {
+                    key = senderM + "_" + recipientM;
+                }
+                else
+                {
+                    key = recipientM + "_" + senderM;
+                }
+
+                List<string> msgs;
+                if (chatHistories.TryGetValue(key, out msgs))
+                {
+
+                    foreach (var item in msgs)
+                    {
+                        
+                            MessageList.Add(item);
+                        
+
+                    }
+                }
+
+            }
+            });
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+public class ChatHistory
+{
+    public string Sender { get; set; }
+    public string Recipient { get; set; }
+    public string Message { get; set; }
+}
+
+
+class Program
+{
+    static ConcurrentBag<TcpClient> clientList = new ConcurrentBag<TcpClient>();
+    static List<string> usernameList = new List<string>();
+    static ConcurrentDictionary<string, List<string>> chatHistory = new ConcurrentDictionary<string, List<string>>();
+
+
+    static async Task Main(string[] args)
+    {
+
+        int port = 7891;
+
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine("Server started. Waiting for connections");
+
+        while (true)
+        {
+            //Accept a new client
+            TcpClient client = await server.AcceptTcpClientAsync();
+
+            //add client to the clientlist
+            clientList.Add(client);
+
+            Console.WriteLine("New Client Connected {0}", client.Client.RemoteEndPoint);
+
+            //Create a thread to handle communication with the client
+            Thread thread = new Thread(new ParameterizedThreadStart(HandleClientAsync));
+            thread.Start(client);
+
+        }
+
+    }
+
+
+
+    static async void HandleClientAsync(object? obj)
+    {
+        //Get the client socket and create a network stream
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+
+
+        //Read the client's message
+        while (true)
+        {
+            //READ
+            byte[] buffer = new byte[1024];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            string[] portId = client.Client.RemoteEndPoint.ToString().Split(':');
+            Console.WriteLine("Received message from {0}:" + message, portId[1]);
+
+            string[] messageParts = message.Split(':');
+
+            if (messageParts[0] == "USERNAME")
+            {
+                // Add the client to the list
+                usernameList.Add(messageParts[1]);
+
+                //Send updated user list
+                string updateUser = "USERNAME:" + string.Join(',', usernameList);
+                byte[] sendBuffer = Encoding.UTF8.GetBytes(updateUser);
+
+                //broadcast message
+                foreach (var c in clientList)
+                {
+                    await c.GetStream().WriteAsync(sendBuffer,0, sendBuffer.Length);
+                }
+            }
+
+            else if (messageParts[0] == "DISCONNECT")
+            {
+                usernameList.Remove(messageParts[1]);
+                //Send updated user list
+                string updateUser = "USERNAME:" + string.Join(',', usernameList);
+                byte[] sendBuffer = Encoding.UTF8.GetBytes(updateUser);
+
+                foreach (var c in clientList)
+                {
+                    await c.GetStream().WriteAsync(sendBuffer, 0, sendBuffer.Length);
+                }
+
+                break;
+            }
+
+            else if (messageParts[0] == "MESSAGE")
+            {
+                //Send updated user list
+                string forwardMessage = message;
+                byte[] sendBuffer = Encoding.UTF8.GetBytes(forwardMessage);
+
+                //broadcast message
+                foreach (var c in clientList)
+                {
+                    await c.GetStream().WriteAsync(sendBuffer, 0, sendBuffer.Length);
+                }
+            }
+        }
+        
+
+    }
+}
+
+
